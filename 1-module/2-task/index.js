96268360ff30a913ab003e662c8b2836dbc41598
Илья Владимиров <flashwot@yandex.ru>
2022-04-20 03:05:53 +0300

    Исправил функцию валидации

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -10,7 +10,7 @@ function print(text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
-  return name !== null && name.split(' ').join('') === name && name.length >= 4;
+  return name !== null && !name.includes(' ') && name.length >= 4;
 }
 
 function sayHello() {
