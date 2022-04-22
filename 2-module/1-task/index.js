fe97750c3bc52db722ff8e4d509cad4ae28bc7eb
Илья Владимиров <flashwot@yandex.ru>
2022-04-22 03:40:02 +0300

    Выполнил задачи по объектам

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,11 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let result = 0;
+  for (let key in salaries) {
+    if (typeof salaries[key] === "number" && 
+        Math.abs(salaries[key]) >= 0 &&
+        Math.abs(salaries[key]) !== Infinity) {
+      result += salaries[key];
+    }
+  }
+  return result;
 }
